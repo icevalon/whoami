4b3c227aaba238f2a0037a1e2fea6108508be3d0
icevalon <icevalon@users.noreply.github.com>
2026-01-21 13:14:01 +0000

    [ci] deploy site c4d87bfc6380fe667aa31ef344630ed803f9ae0a

---
+++ assets/use-pagetitle-Bbaofa2D.js
@@ -0,0 +1,1 @@
+import{r as t,K as s}from"./index-DNtN6EK2.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-CmQ-GMow.js
+++
@@ -1,1 +0,0 @@
-import{r as t,K as s}from"./index-D3jzGmGx.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
