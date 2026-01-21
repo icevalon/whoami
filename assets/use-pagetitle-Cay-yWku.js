5e67666c812bb2ade7df675ca7fc6877ef9672fe
icevalon <icevalon@users.noreply.github.com>
2026-01-21 17:33:19 +0000

    [ci] deploy site 6eaed8f09997247cf0c7f02bb85761d842b9e717

---
+++ assets/use-pagetitle-Cay-yWku.js
@@ -0,0 +1,1 @@
+import{r as t,K as s}from"./index-C_6f1AMb.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-kHG_Dumg.js
+++
@@ -1,1 +0,0 @@
-import{r as t,K as s}from"./index-BTed-yb6.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
